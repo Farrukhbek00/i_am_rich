f991fb9d9f5926e5a0facd0e5c56990d85e1af77
Farrukhbek <farruhzokirov00@gmail.com>
2020-02-05 14:38:24 +0500

    worked with assets

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-        home: Scaffold(
-            backgroundColor: Color(0xff84FFFF),
-            appBar: AppBar(
-              title: Text('I am Rich'),
-              backgroundColor: Colors.black45,
-            ),
-            body: Center(
-                child: Image(
-              image: NetworkImage(
-                  'https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg'),
-            )))),
+      home: Scaffold(
+        backgroundColor: Color(0xff84FFFF),
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.black45,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/i_am_rich_app_icon.png'),
+          ),
+        ),
+      ),
+    ),
   );
 }
